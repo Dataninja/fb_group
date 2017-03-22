7bc8dae4c127e74c8dcfed2adfb2df4354e7bbf0
Alessio Cimarelli <jenkin@dataninja.it>
2017-03-22 19:57:11 +0100

    Data are represented by a semantic network, with users, posts, comments as nodes and directed and labeled edges

--- fb_group.py
+++ fb_group.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import networkx as nx
 
 fb_datetime_format = "%Y-%m-%dT%H:%M:%S" # 2017-03-21T14:18:11+0000
-G = nx.Graph()
+G = nx.DiGraph()
 
 if len(sys.argv) > 1:
     config_file = sys.argv[1]
@@ -87,12 +87,9 @@ for member in members:
     logging.debug(member)
     num_members += 1
 
-    G.add_node(member['id'], node_type = 'user', **member) # user
+    G.add_node(member['id'], type = 'user', **member) # user
 
     logging.info("- MEMBER: %s" % member['name'])
-
-    if not num_members%25:
-        break
 
 posts = graph.get_all_connections(
     id = group_id,
@@ -118,12 +115,12 @@ for post in posts:
     if post_datetime < since_datetime:
         break
 
-    G.add_node(post['id'], node_type = 'post', **post) # post
-    G.add_node(post['from']['id'], node_type = 'user', **post['from']) # user
+    G.add_node(post['id'], type = 'post', **post) # post
+    G.add_node(post['from']['id'], type = 'user', **post['from']) # user
 
-    G.add_edge(post['id'], post['from']['id']) # post <--> user
+    G.add_edge(post['from']['id'], post['id'], type = 'is author of') # user -|is author of|> post
 
-    logging.info("- POST: %s" % post['message'])
+    logging.info("- POST: %s" % post.get('message',''))
     logging.info("+ DATETIME: %s" % post['updated_time'])
     logging.info("+ AUTHOR: %s" % post['from']['name'])
 
@@ -137,11 +134,11 @@ for post in posts:
         logging.debug(reaction)
         num_reactions += 1
 
-        G.add_node(reaction['type'], node_type = 'reaction') # reaction
-        G.add_node(reaction['id'], node_type = 'user', id = reaction['id'], name = reaction['name']) # user
+        #G.add_node(reaction['type'], type = 'reaction') # reaction
+        G.add_node(reaction['id'], type = 'user', id = reaction['id'], name = reaction['name']) # user
 
-        G.add_edge(reaction['type'], post['id']) # reaction <--> post
-        G.add_edge(reaction['type'], reaction['id']) # reaction <--> user
+        #G.add_edge(reaction['type'], post['id']) # reaction <--> post
+        G.add_edge(reaction['id'], post['id'], type = 'reacts to', reaction = reaction['type']) # user -|reacts to|> post
 
         logging.info("+ %s: %s" % ( reaction['type'] , reaction['name']))
 
@@ -155,13 +152,13 @@ for post in posts:
         logging.debug(comment)
         num_comments += 1
 
-        G.add_node(comment['id'], node_type = 'comment', **comment) # comment
-        G.add_node(comment['from']['id'], node_type = 'user', **comment['from']) # user
+        G.add_node(comment['id'], type = 'comment', **comment) # comment
+        G.add_node(comment['from']['id'], type = 'user', **comment['from']) # user
 
-        G.add_edge(comment['id'], post['id']) # comment <--> post
-        G.add_edge(comment['id'], comment['from']['id']) # comment <--> user
+        G.add_edge(comment['id'], post['id'], type = 'in reply to') # comment -|in reply to|> post
+        G.add_edge(comment['from']['id'], comment['id'], type = 'is author of') # user -|is author of|> comment
 
-        logging.info("-- COMMENT: %s" % comment['message'])
+        logging.info("-- COMMENT: %s" % comment.get('message',''))
         logging.info("++ DATETIME: %s" % comment['created_time'])
         logging.info("++ AUTHOR: %s" % comment['from']['name'])
 
@@ -175,49 +172,49 @@ for post in posts:
             logging.debug(like)
             num_reactions += 1
 
-            G.add_node('LIKE', node_type = 'reaction') # reaction
-            G.add_node(like['id'], node_type = 'user', **like) # user
+            #G.add_node('LIKE', type = 'reaction') # reaction
+            G.add_node(like['id'], type = 'user', **like) # user
 
-            G.add_edge('LIKE', comment['id']) # reaction <--> comment
-            G.add_edge('LIKE', like['id']) # reaction <--> user
+            #G.add_edge('LIKE', comment['id']) # reaction <--> comment
+            G.add_edge(like['id'], comment['id'], type = 'reacts to', reaction = 'LIKE') # user -|reacts to|> comment
 
             logging.info("++ LIKE: %s" % like['name'])
 
-        responses = graph.get_all_connections(
+        replies = graph.get_all_connections(
             id = comment['id'],
             connection_name = "comments"
         )
 
-        for response in responses:
+        for reply in replies:
 
-            logging.debug(response)
+            logging.debug(reply)
             num_comments += 1
 
-            G.add_node(response['id'], node_type = 'comment', **response) # comment
-            G.add_node(response['from']['id'], node_type = 'user', **response['from']) # user
+            G.add_node(reply['id'], type = 'comment', **reply) # comment
+            G.add_node(reply['from']['id'], type = 'user', **reply['from']) # user
 
-            G.add_edge(response['id'], comment['id']) # comment <--> comment
-            G.add_edge(response['id'], response['from']['id']) # comment <--> user
+            G.add_edge(reply['id'], comment['id'], type = 'in reply to') # comment -|in reply to|> comment
+            G.add_edge(reply['from']['id'], reply['id'], type = 'is author of') # user -|is author of|> comment
 
-            logging.info("--- RESPONSE: %s" % response['message'])
-            logging.info("+++ DATETIME: %s" % response['created_time'])
-            logging.info("+++ AUTHOR: %s" % response['from']['name'])
+            logging.info("--- reply: %s" % reply.get('message',''))
+            logging.info("+++ DATETIME: %s" % reply['created_time'])
+            logging.info("+++ AUTHOR: %s" % reply['from']['name'])
 
-            response_likes = graph.get_all_connections(
-                id = response['id'],
+            reply_likes = graph.get_all_connections(
+                id = reply['id'],
                 connection_name = "likes"
             )
 
-            for like in response_likes:
+            for like in reply_likes:
 
                 logging.debug(like)
                 num_reactions += 1
 
-                G.add_node('LIKE', node_type = 'reaction') # reaction
-                G.add_node(like['id'], node_type = 'user', **like) # user
+                #G.add_node('LIKE', type = 'reaction') # reaction
+                G.add_node(like['id'], type = 'user', **like) # user
 
-                G.add_edge('LIKE', response['id']) # reaction <--> comment
-                G.add_edge('LIKE', like['id']) # reaction <--> user
+                #G.add_edge('LIKE', reply['id']) # reaction <--> comment
+                G.add_edge(like['id'], reply['id'], type = 'reacts to', reaction = 'LIKE') # user -|reacts to|> comment
 
                 logging.info("+++ LIKE: %s" % like['name'])
 
@@ -244,8 +241,15 @@ except ConfigParser.NoOptionError, e:
 
 if graph_type == 'json':
     from networkx.readwrite import json_graph
+    json_data = json_graph.node_link_data(G)
+    # node_link_data() sets links' target and source to nodes' indices in nodes array,
+    # not to real nodes' ids... this workaround fix this issue
+    for i,l in enumerate(json_data['links']):
+        json_data['links'][i]['target'] = json_data['nodes'][l['target']]['id']
+        json_data['links'][i]['source'] = json_data['nodes'][l['source']]['id']
+    # end workaround
     with open(file_name,'w') as f:
-        json.dump(json_graph.node_link_data(G), f)
+        json.dump(json_data, f)
 else:
     try:
         write_graph = getattr(nx, "write_%s" % graph_type)
